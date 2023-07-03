8b2531eaaae8ea3014dabf0b7b256f4530b9c9c0
laiba-javaid <laibajavaid423@gmail.com>
2023-07-03 15:21:02 +0500

    drawer navigation

--- LittleApp/App.js
+++ LittleApp/App.js
@@ -1,46 +1,34 @@
 import * as React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import { Ionicons } from '@expo/vector-icons';
 //import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//import LittleLemonHeader from './LittleLemonHeader';
-//import LittleLemonFooter from './LittleLemonFooter';
+import LittleLemonHeader from './LittleLemonHeader';
+import LittleLemonFooter from './LittleLemonFooter';
 import MenuScreen from "./MenuScreen";
 import WelcomeScreen from "./WelcomeScreen";
-//import LoginScreen from './LoginScreen';
+import LoginScreen from './LoginScreen';
 
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <>
     <NavigationContainer>
-    <Tab.Navigator screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Home') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline';
-          } else if (route.name === 'Menu') {
-            iconName =  'ios-list';
-          }
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      })}>
-        <Tab.Screen name="Home" component={WelcomeScreen} />
-        <Tab.Screen name="Menu" component={MenuScreen} />
-      </Tab.Navigator>
-
-
-      {/* <View style={styles.footerContainer}>
+    <View style={styles.container}>
+    <LittleLemonHeader />
+    
+    <Drawer.Navigator useLegacyImplementation initialRouteName="Login">
+            <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+            <Drawer.Screen name="Login" component={LoginScreen} />
+    </Drawer.Navigator>
+    </View>
+    <View style={styles.footerContainer}>
           <LittleLemonFooter />
-        </View> */}
+    </View>
     </NavigationContainer>
     </>
   );
